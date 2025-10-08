1315b1361b7df69f05fd1494a294c429ba47a2cc
Steven Grissom <sggrissom@gmail.com>
2025-10-08 17:40:04 -0500

    redirect to homepage if not authenticated

--- frontend/pages/dashboard/dashboard.tsx
+++ frontend/pages/dashboard/dashboard.tsx
@@ -1,13 +1,22 @@
 import * as preact from "preact";
 import * as rpc from "vlens/rpc";
+import * as core from "vlens/core";
+import * as server from "../../server";
 import { Header, Footer } from "../../layout";
 import "../../styles/global";
 import "./dashboard-styles";
 
-type Data = {};
+type Data = {
+  authId: number;
+};
 
 export async function fetch(route: string, prefix: string) {
-  return rpc.ok<Data>({});
+  // Check if user is authenticated
+  let [authResp, authErr] = await server.GetAuthContext({});
+
+  return rpc.ok<Data>({
+    authId: authResp?.id || 0,
+  });
 }
 
 export function view(
@@ -15,6 +24,12 @@ export function view(
   prefix: string,
   data: Data,
 ): preact.ComponentChild {
+  // Redirect to site root if not authenticated
+  if (data.authId === 0) {
+    core.setRoute("/");
+    return <div></div>;
+  }
+
   return (
     <div>
       <Header />
